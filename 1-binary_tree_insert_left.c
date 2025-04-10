4055a7f6792a3d9a167a1384cacd89b0251efd75
HISvessel <kevinsanchezdabest@yahoo.com>
2025-04-10 00:27:07 -0400

    this function adds a node to the left side of a node and also to the left side of a left child node and shifts the old one down without overwriting it: when we think of pointers, we shift them with temporary data pointers to make the transition smooth. Here is the most important parts, however: first, if the child node does not point to its parental node, thechain is incomplete and unexpected behaviour could arise. and 2, undefined behaviour will occurr if one tries to insert a child node with a null parent pointer

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - this function adds nodes to the left of root
+ * @parent: the root node of the tree
+ * @value: data stored in node
+ * Return: the tree node
+ */
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *node = malloc(sizeof(binary_tree_t));
+	binary_tree_t *left_temp;
+
+	if (node == NULL)
+	{
+		return (NULL);
+	}
+	node->n = value;
+	node->parent = parent;
+	if (parent->left == NULL)
+	{
+		return (NULL);
+	}
+	left_temp = parent->left;
+	parent->left = node;
+	node->left = left_temp;
+	left_temp->parent = node;
+	return (node);
+}
