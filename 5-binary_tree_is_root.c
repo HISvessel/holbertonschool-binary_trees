75edfbd51f4420889188abdedf41baffcac02710
HISvessel <kevinsanchezdabest@yahoo.com>
2025-04-12 00:15:53 -0400

    this function checks to see if the node is the root or if it is a child or a leaf. Following the same concept of our last functino in task 4, we set a big distinction fro this condition here, and that is to say that instead of left and right subtrees being NULL, that we should check if they AREN'T NULL

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -14,9 +14,5 @@ int binary_tree_is_root(const binary_tree_t *node)
 	{
 		return (0);
 	}
-	if (node->left && node->right)
-	{
-		return (0);
-	}
-	return (1);
+		return (node && node->parent == NULL);
 }
