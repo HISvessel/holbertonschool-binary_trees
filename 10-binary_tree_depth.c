ecab3addfde0e29b9cbbd48d465219355d150e89
HISvessel <kevinsanchezdabest@yahoo.com>
2025-04-13 20:41:27 -0400

    this function follows a pointer to the leaf nodes of the tree  and returns the depth size in size_t. This is a test for the checker more so, I have no doubt that this should work, but we shall see

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - this function traverses and checks how deep is the tree
+ * @tree: the pointer to a tree structure
+ * Return: size of node depth, or 0
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t depth = 0;
+
+	if (!tree)
+	{
+		return (0);
+	}
+	if (tree->parent != NULL)
+	{
+		depth = 1 + binary_tree_depth(tree->parent);
+	}
+	return (depth);
+}
